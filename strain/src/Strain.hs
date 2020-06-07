8b166efcfcd47b6595e0d9ba71fe7ea7599e6b42
Justin Dennison <justin1dennison@gmail.com>
2020-06-06 20:28:25 -0400

    played by the rules for strain

--- strain/src/Strain.hs
+++ strain/src/Strain.hs
@@ -1,7 +1,10 @@
 module Strain (keep, discard) where
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard p xs = filter (not . p) xs
+discard _ [] = []
+discard p (x:xs) 
+    | p x = discard p xs
+    | otherwise = x : discard p xs
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep p xs = filter p xs
+keep p xs = discard (not . p) xs
