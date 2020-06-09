192dabc8134fbc187e4c0dab66106e9a3b81cb43
Justin Dennison <justin1dennison@gmail.com>
2020-06-09 12:56:11 -0400

    refactor of bob

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -5,7 +5,7 @@ import qualified Data.Char as C
 responseFor :: String -> String
 responseFor xs 
   | isYelling xs && isQuestion xs = "Calm down, I know what I'm doing!"
-  | null $ filter (not . C.isSpace) xs = "Fine. Be that way!"
+  | not (any (not . C.isSpace) xs) = "Fine. Be that way!"
   | isYelling xs = "Whoa, chill out!"
   | isQuestion xs = "Sure."
   | otherwise = "Whatever."
@@ -13,8 +13,8 @@ responseFor xs
 isYelling :: String -> Bool
 isYelling xs  
   | null filtered = False
-  | otherwise =  and $ map C.isUpper $ filter C.isAlpha xs
+  | otherwise =  all C.isUpper $ filter C.isAlpha xs
   where filtered = filter C.isAlpha xs 
 
 isQuestion :: String -> Bool
-isQuestion xs = (== '?') $ head $ reverse $ filter (not . C.isSpace) xs
+isQuestion xs = (== '?') $ last $ filter (not . C.isSpace) xs
