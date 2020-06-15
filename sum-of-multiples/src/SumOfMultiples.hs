3a7188d1b76efe262675754af4ddec0d1ee15437
Justin Dennison <justin1dennison@gmail.com>
2020-06-15 15:06:38 -0400

    updated to utilize set for dedupe of elements

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,8 +1,8 @@
 module SumOfMultiples (sumOfMultiples) where
 
-import Data.List (nub)
+import Data.Set (fromList)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit = f $ map g factors
-  where f = sum . nub . concat
+  where f = sum . fromList . concat
         g n = if n == 0 then [] else [0, n .. limit - 1]
