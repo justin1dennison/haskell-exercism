6043dac02fb3a18ecfe4d70438b4d1cc0a7a7279
Justin Dennison <justin1dennison@gmail.com>
2020-06-25 21:27:31 -0400

    refactor of the largest series product

--- largest-series-product/src/Series.hs
+++ largest-series-product/src/Series.hs
@@ -8,6 +8,9 @@ data Error = InvalidSpan
            | InvalidDigit Char
                deriving (Show, Eq)
 
+isNotDigit :: Char -> Bool
+isNotDigit = not . isDigit
+
 windows :: Int -> [a] -> [[a]]
 windows n = filter ((== n) . length) . transpose . take n . tails
 
@@ -15,8 +18,8 @@ largestProduct :: Int -> String -> Either Error Integer
 largestProduct size digits
   | size == 0 = Right 1
   | size > length digits || size < 0 = Left InvalidSpan
-  | (not . all isDigit) digits =
-    Left (InvalidDigit $ fromJust $ find (not . isDigit) digits)
+  | any isNotDigit digits =
+    Left (InvalidDigit $ fromJust $ find isNotDigit digits)
   | otherwise =
     Right $
       toInteger $ maximum $ map product $ windows size $ map digitToInt digits
